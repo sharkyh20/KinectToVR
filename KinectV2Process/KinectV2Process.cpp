0ce27166c158b1d5e9f48146e2281b79ec60e882
sharkyh20 <dooger1@live.com.au>
2018-04-14 13:51:37 +1000

    Removed Console WIndow when compiling with Release

--- KinectV2Process/KinectV2Process.cpp
+++ KinectV2Process/KinectV2Process.cpp
@@ -13,3 +13,14 @@ int main()
     return 0;
 }
 
+#ifdef _WIN32
+// This disables the console window from appearing on windows only if the Project Settings->Linker->System->SubSystem is set to Windows (rather than Console).
+int WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCMDShow)
+{
+    KinectV2Handler kinect;
+
+    processLoop(kinect);
+
+    return 0;
+}
+#endif
