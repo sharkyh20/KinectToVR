3cacd7f7a6c10a17725e1b3aaa63e05490a81d8a
sharkyh20 <dooger1@live.com.au>
2018-10-22 15:02:02 +1100

    Minor Compilation and Include Issues

--- KinectV2Process/SmoothingParameters.cpp
+++ KinectV2Process/SmoothingParameters.cpp
@@ -1,12 +1,13 @@
 #include "stdafx.h"
 #include "SmoothingParameters.h"
-struct SmoothingParameters {
-    float smoothing = .25f;    // [0..1], lower values closer to raw data
-    float correction = .25f;   // [0..1], lower values slower to correct towards the raw data
-    float prediction = .05f;   // [0..n], the number of frames to predict into the future
-    float jitterRadius = 0.03f; // The radius in meters for jitter reduction
-    float maxDeviationRadius = .25f; // The maximum radius in meters that filtered positions are allowed to deviate from raw data
-};
+
+
+//m_fSmoothing How much soothing will occur.  Will lag when too high
+//m_fCorrection How much to correct back from prediction.  Can make things springy
+//m_fMaxDeviationRadius Size of the max prediction radius Can snap back to noisy data when too high
+//m_fJitterRadius Size of the radius where jitter is removed. Can do too much smoothing when too high
+//m_fPrediction Amount of prediction into the future to use. Can over shoot when too high
+
 SmoothingParameters getDefaultSmoothingParams() {
     SmoothingParameters params;
     params.smoothing = .25f;
--- KinectV2Process/SmoothingParameters.h
+++ KinectV2Process/SmoothingParameters.h
@@ -5,11 +5,11 @@
 #include "Kinect.h"
 
 struct SmoothingParameters {
-    float smoothing;    // [0..1], lower values closer to raw data
-    float correction;   // [0..1], lower values slower to correct towards the raw data
-    float prediction;   // [0..n], the number of frames to predict into the future
-    float jitterRadius; // The radius in meters for jitter reduction
-    float maxDeviationRadius; // The maximum radius in meters that filtered positions are allowed to deviate from raw data
+    float smoothing = .25f;    // [0..1], lower values closer to raw data
+    float correction = .25f;   // [0..1], lower values slower to correct towards the raw data
+    float prediction = .05f;   // [0..n], the number of frames to predict into the future
+    float jitterRadius = 0.03f; // The radius in meters for jitter reduction
+    float maxDeviationRadius = .25f; // The maximum radius in meters that filtered positions are allowed to deviate from raw data
 };
 SmoothingParameters getDefaultSmoothingParams();
 SmoothingParameters getAggressiveSmoothingParams();
