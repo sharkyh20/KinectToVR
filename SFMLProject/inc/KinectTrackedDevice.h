356b92eda0d39dac30f32de25d8de0a5f020bd02
sharkyh20 <dooger1@live.com.au>
2018-12-21 22:30:07 +1100

    More offsets, and refixing rotation

--- SFMLProject/inc/KinectTrackedDevice.h
+++ SFMLProject/inc/KinectTrackedDevice.h
@@ -137,7 +137,11 @@ namespace KVR {
                 nextUpdatePose.vecWorldFromDriverTranslation[1] -= KinectSettings::secondaryTrackingOriginOffset.v[1];
                 nextUpdatePose.vecWorldFromDriverTranslation[2] -= KinectSettings::secondaryTrackingOriginOffset.v[2];
 
-                nextUpdatePose.qDriverFromHeadRotation = vrmath::inverse(GetVRRotationFromMatrix(KinectSettings::trackingOrigin)); // CLEAN UP INTO SETTINGS
+                nextUpdatePose.vecPosition[0] -= 0.98954;
+                nextUpdatePose.vecPosition[2] -= 0.207021;
+
+
+                nextUpdatePose.qWorldFromDriverRotation = vrmath::inverse(GetVRRotationFromMatrix(KinectSettings::trackingOrigin)); // CLEAN UP INTO SETTINGS
                 update(nextUpdatePose);
                 
                 nextUpdatePoseIsSet = false;
